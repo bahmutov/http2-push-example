d9a144ea45f1db2bb3d463151b88df145c7d6e33
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-01-07 10:59:00 -0500

    read files on startup

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const morgan = require('morgan')
 
 const app = express()
 app.use(morgan('dev'))
-app.get('/', serveHome)
-app.use(express.static('public'))
 
 function readFiles () {
   const files = [
@@ -28,29 +26,23 @@ const imageOptions = {
   res: {'content-type': 'image/jpeg'}
 }
 
-function serveHome (req, res) {
-  console.log('serving home')
+function serveHome (index, image1, image2) {
+  console.log('making home page middleware')
+  return function (req, res) {
+    console.log('serving home')
 
-  const homePageWithPush = files => {
     if (res.push) {
       console.log('browser supports HTTP/2 Push!!!',
         'is SPDY?', req.isSpdy, 'spdy version', req.spdyVersion)
-      pushFile('/images/image1.jpg', files[1], imageOptions, res)
-      pushFile('/images/image2.jpg', files[2], imageOptions, res)
+      pushFile('/images/image1.jpg', image1, imageOptions, res)
+      pushFile('/images/image2.jpg', image2, imageOptions, res)
     } else {
       console.log('No HTTP/2 Push :(, is page secure?',
         req.secure, 'is SPDY?', req.isSpdy)
     }
-
-    // index.html is the first file
     res.writeHead(200)
-    res.end(files[0])
+    res.end(index)
   }
-
-  // we could also read all files at startup
-  Promise.all(readFiles())
-    .then(homePageWithPush)
-    .catch(error => res.status(500).send(error.toString()))
 }
 
 const tlsOptions = {
@@ -61,10 +53,20 @@ const tlsOptions = {
     ssl: true
   }
 }
-const port = process.env.PORT || 5000
-server.createServer(tlsOptions, app).listen(port, err => {
-  if (err) {
-    throw new Error(err)
-  }
-  console.log('listening on port', port)
-})
+
+function startServer (files) {
+  app.get('/', serveHome(files[0], files[1], files[2]))
+  app.use(express.static('public'))
+
+  const port = process.env.PORT || 5000
+  server.createServer(tlsOptions, app).listen(port, err => {
+    if (err) {
+      throw new Error(err)
+    }
+    console.log('listening on port', port)
+  })
+}
+
+Promise.all(readFiles())
+  .then(startServer)
+  .catch(error => console.error(error.toString()))
